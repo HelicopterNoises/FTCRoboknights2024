8e033016ff6541139157df48fb949f06d59b4fbc
Cedric <cedric.ye9@gmail.com>
2024-12-22 17:28:38 -0500

    Added SlideLock (this has not been uploaded to the robot as of 12/22)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -118,7 +89,8 @@ public class BasicOmniOpMode extends LinearOpMode {
         telemetry.update();
         double speed = 0.6;//EH Slot 0
         double sliderailPower = 0.0;
-        double clawPos = 0.0;
+        double targetposition = 0.0;
+        boolean slideLock = false;
 
 
         waitForStart();
@@ -205,26 +177,49 @@ public class BasicOmniOpMode extends LinearOpMode {
                 winch.setPower(0);
             }
 
-            if (gamepad1.dpad_up & ((sliderailPos >= -2100) || (encoderBlock = false))) { // "\\" means or
+            //The following code is Sliderail controls with encoder blockers and SlideLock, which ensures the sliderail doesn't fall when extended, even at high angles
+            //Slidelock is not good for the motor so it is minimized as much as possible
+            if (gamepad1.dpad_up & ((sliderailPos >= -2100) || (encoderBlock = false))) { //Raises sliderail, but does not allow overextension IF enabled (disabled via left bumper)
+                sliderail.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                 sliderailPower = -0.5;
-            } else if (gamepad1.dpad_down & ((sliderailPos <= -100) || (encoderBlock = true))) {
-                sliderailPower = 0.5;
-            } else {
+                targetposition = sliderail.getCurrentPosition(); //This creates a variable so IF the sliderail falls, there will be a mismatch, and the sliderail will move back and hold its position
+                slideLock = true;
+            }
+            else if (gamepad1.dpad_down) {
+                sliderail.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                sliderailPower = 0.3;
+                targetposition = sliderail.getCurrentPosition();
+            }
+            else if (gamepad1.dpad_right){ //press to manually disable slidelock after each extension if not needed to preserve motor
+                slideLock = false;
+                targetposition = sliderail.getCurrentPosition();
                 sliderailPower = 0;
             }
-
-            if (sliderailPos <= -2100 || sliderailPos >= -100) {
-                telemetry.addData("you're going too far friendo ", sliderailPos);
+            else if (slideLock == true) { //WHEN SLIDELOCK ENABLED, THIS BLOCK HOLDS THE SLIDERAIL AT ITS POSITION
+                sliderailPower = 0;
+                sliderail.setTargetPosition((int)targetposition); //to make motor run to location, set target position
+                sliderail.setMode(DcMotor.RunMode.RUN_TO_POSITION);//then tells it to move to the location it was originally extended to
+                sliderailPower = 0.25; //this specifies maximum power it can run at to correct
+            }
+            else { //If all conditions is false, don't run. This is probably not neccesary
+                sliderailPower = 0;
+                slideLock = false;
             }
             sliderail.setPower(sliderailPower);
+
+            if (sliderailPos <= -2100) { //telemetry if sliderail is overextended
+                telemetry.addData("you're going too far friendo (disable with left bumper) ", sliderailPos);
+            }
+            else {
+                telemetry.addData("EncoderBlock (disable w/ left bumper): ", encoderBlock);
+            }
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.addData("Winch position: ", winchPos); //sends winch and sliderail position to the driver (for testing)
+            telemetry.addData("Slidelock (disable w/ Right Dpad): ", slideLock);
             telemetry.addData("Sliderail distance: ", sliderailDistance);
-            telemetry.addData("Winch Angle", winchAngle);
             telemetry.update();
 
 
